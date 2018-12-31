2ea533f2f4021d80aa9785dbbb8adda9c362b70a
Lex417 <leo.hidalgo95@gmail.com>
2018-12-31 16:32:23 -0600

    Cambios menores al inicio

--- web-manager-back-end/frontend/views/site/about.php
+++ web-manager-back-end/frontend/views/site/about.php
@@ -10,7 +10,17 @@ $this->params['breadcrumbs'][] = $this->title;
 <div class="site-about">
     <h1><?= Html::encode($this->title) ?></h1>
 
-    <p>This is the About page. You may modify the following file to customize its content:</p>
+    <p>ESCUELA DE INFORMATICA UNA.
+       CURSO: EIF409 APLICACIONES INFORMATICAS GLOBALES.
+       <br>
+       ESTUDIANTES:
+       <br>
+        <br>Dennis Bonilla Ramírez.
+        <br>Leonardo Hidalgo Fonseca.
+        <br>Nathalia Ovares.
+        <br>Eithan Méndez Méndez.
+        <br> Didier Ocampo Martínez.
+        <br>Yaudi Somarribas Araya.</p>
 
     <code><?= __FILE__ ?></code>
 </div>
--- web-manager-back-end/frontend/views/site/index.php
+++ web-manager-back-end/frontend/views/site/index.php
@@ -2,51 +2,32 @@
 
 /* @var $this yii\web\View */
 
-$this->title = 'My Yii Application';
+$this->title = 'Web Manager';
 ?>
 <div class="site-index">
 
     <div class="jumbotron">
-        <h1>Congratulations!</h1>
+        <h1>Welcome to Web Manager!</h1>
 
-        <p class="lead">You have successfully created your Yii-powered application.</p>
+        <p class="lead">The tool for every team and project manager.</p>
 
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
+        <p><a class="btn btn-lg btn-success" href="https://github.com/dennisbonilla/web-manager">Tool's Github</a></p>
     </div>
 
     <div class="body-content">
 
         <div class="row">
-            <div class="col-lg-4">
-                <h2>Heading</h2>
+            <div class="col-lg-8">
+                <h2>Web-Manager</h2>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
+                <p>A powerful tool to control the creation of new projects within a company, managers would be able to request
+                   workers that better suit the needs of an emergent project based on the skills required for each position, 
+                   the user also would have the information of all employees categorized by their disciplinaries areas, 
+                   each worker would have a presentation card with all the projects they have participated, an skillset and basic information.</p>
 
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
+                <p><a class="btn btn-default" href="https://github.com/dennisbonilla/web-manager"> Documentation &raquo;</a></p>
             </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
-            </div>
+           
         </div>
 
     </div>

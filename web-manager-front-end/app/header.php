a6f2093f59571cf95c7086b047dfd73d874e32cc
NanaOva <nathy.ovares@gmal.com>
2019-01-02 14:02:27 -0600

    Agregada vista de dashboard

---
+++ web-manager-front-end/app/header.php
@@ -0,0 +1,51 @@
+<header class="header-desktop">
+                <div class="section__content section__content--p30">
+                    <div class="container-fluid">
+                        
+                        <div class="header-wrap">
+                            <div class="form-header">
+                            
+                            </div>
+                            <div class="header-button">
+                                
+                                <div class="account-wrap">
+                                    <div class="account-item clearfix js-item-menu">
+                                        <div class="image">
+                                            <img src="images/user.ico" alt="User" />
+                                        </div>
+                                        <div class="content">
+                                            <a class="js-acc-btn" href="#">User</a>
+                                        </div>
+                                        <div class="account-dropdown js-dropdown">
+                                            <div class="info clearfix">
+                                                <div class="image">
+                                                    <a href="#">
+                                                        <img src="images/user.ico" alt="User" />
+                                                    </a>
+                                                </div>
+                                                <div class="content">
+                                                    <h5 class="name">
+                                                        <a href="#">User</a>
+                                                    </h5>
+                                                    <span class="email">user@example.com</span>
+                                                </div>
+                                            </div>
+                                            <div class="account-dropdown__body">
+                                                <div class="account-dropdown__item">
+                                                    <a href="#">
+                                                        <i class="zmdi zmdi-account"></i>Ver Perfil</a>
+                                                </div>
+                                            </div>
+                                            <div class="account-dropdown__footer">
+                                                <a href="#">
+                                                    <i class="zmdi zmdi-power"></i>Cerrar Sesión</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </header>
+            <!-- HEADER DESKTOP-->
---
+++ web-manager-front-end/app/menu.php
@@ -0,0 +1,90 @@
+<header class="header-mobile d-block d-lg-none">
+            <div class="header-mobile__bar">
+                <div class="container-fluid">
+                    <div class="header-mobile-inner">
+                        <a class="logo" href="index.html">
+                            <img src="images/logo/logo.png" width="120px" height="30px" alt="WebManager" />
+                        </a>
+                        <button class="hamburger hamburger--slider" type="button">
+                            <span class="hamburger-box">
+                                <span class="hamburger-inner"></span>
+                            </span>
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <nav class="navbar-mobile">
+                <div class="container-fluid">
+                    <ul class="navbar-mobile__list list-unstyled">
+                        <li>
+                            <a href="chart.html">
+                                <i class="fas fa-chart-bar"></i>Inicio</a>
+                        </li>
+                        <li>
+                            <a href="table.html">
+                                <i class="fas fa-table"></i>Información</a>
+                        </li>
+                        <li class="has-sub">
+                            <a class="js-arrow" href="#">
+                                <i class="fas fa-tachometer-alt"></i>Dashboard</a>
+                            <ul class="navbar-mobile-sub__list list-unstyled js-sub-list">
+                                <li>
+                                    <a href="index.html">Proyecto  1</a>
+                                </li>
+                                <li>
+                                    <a href="index2.html">Proyecto  2</a>
+                                </li>
+                               
+                            </ul>
+                        </li>
+                        
+                        
+                        
+                        
+                    </ul>
+                </div>
+            </nav>
+        </header>
+        <!-- END HEADER MOBILE-->
+
+        <!-- MENU SIDEBAR-->
+        <aside class="menu-sidebar d-none d-lg-block">
+            <div class="logo">
+                <a href="#">
+                    <img src="images/logo/logo.png" width="150px" height="50px" alt="WebManager"  />
+                </a>
+            </div>
+            <div class="menu-sidebar__content js-scrollbar1">
+                <nav class="navbar-sidebar">
+                    <ul class="list-unstyled navbar__list">
+                        
+                        <li>
+                            <a href="chart.html">
+                                <i class="fas fa-chart-bar"></i>Inicio</a>
+                        </li>
+                        <li>
+                            <a href="table.html">
+                                <i class="fas fa-table"></i>Información</a>
+                        </li>
+                        <li class="active has-sub">
+                            <a class="js-arrow" href="#">
+                                <i class="fas fa-tachometer-alt"></i>Dashboard</a>
+                            <ul class="list-unstyled navbar__sub-list js-sub-list">
+                                <li>
+                                    <a href="index.html">Proyecto  1</a>
+                                </li>
+                                <li>
+                                    <a href="index2.html">Proyecto 2</a>
+                                </li>
+                                
+                            </ul>
+                        </li>
+                        
+                        
+                        
+                       
+                    </ul>
+                </nav>
+            </div>
+        </aside>
+        <!-- END MENU SIDEBAR-->
